f3a07d625397fbc97c20e26404fa25e323558d10
Triple-V8 <3plev.eth@Victors-MacBook-Pro.local>
2023-06-04 17:54:16 +0100

    Fixed the first row sticky issue, About to start the header part

--- src/components/HomePageComponents/Home.tsx
+++ src/components/HomePageComponents/Home.tsx
@@ -71,19 +71,6 @@ const HomeComnponent = () => {
     const [originalARR, setOriginalARR] = useState([]);
     const [tabState, setTabState] = useState("all coins");
 
-
-    const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-                    11 ,12, 13, 14, 15, 16, 17, 18, 19, 20,
-                    21 ,22, 23, 24, 25, 26, 27, 28, 29, 30,
-                    31 ,32, 33, 34, 35, 36, 37, 38, 39, 40,
-                    41 ,42, 43, 44, 45, 46, 47, 48, 49, 50,
-                    51 ,52, 53, 54, 55, 56, 57, 58, 59, 60,
-                    61 ,62, 63, 64, 65, 66, 67, 68, 69, 70,
-                    71 ,72, 73, 74, 75, 76, 77, 78, 79, 80,
-                    81 ,82, 83, 84, 85, 86, 87, 88, 89, 90,
-                    91 ,92, 93, 94, 95, 96, 97, 98, 99, 100
-                ];
-
                 const offset = 3; // Offset for generating pagev array
 
                 // Calculate the minimum and maximum bounds based on the given range
@@ -109,6 +96,20 @@ const HomeComnponent = () => {
     const toast = useToast();
     const dispacth = useDispatch();
 
+    const headerRef = useRef<HTMLDivElement>(null);
+    const [isHeaderSticky, setIsHeaderSticky] = useState(false);
+    
+    let checkStick = () => {
+        const headerElement = headerRef.current;
+            if (headerElement) {
+          const rect = headerElement.getBoundingClientRect();
+          setIsHeaderSticky(rect.top <= 0);
+            
+        };
+      }
+      if (typeof window !== 'undefined') {
+        window.addEventListener('scroll', checkStick);
+      }
 
     //fecthnig data
     useEffect(() => {
@@ -371,7 +372,7 @@ const HomeComnponent = () => {
                     :
                     <table className="table cryptoDataMainTable mx-auto w-full">
                         {/* head */}
-                        <thead>
+                        <thead className={`header ${isHeaderSticky ? 'sticky' : ''}`}>
                             <tr>
                                 <th></th>
                                 <th className='metaverse-table'>Coin</th>
@@ -397,8 +398,7 @@ const HomeComnponent = () => {
                                 </th>
                             </tr>
                         </thead>
-
-                        <tbody>
+                        <tbody ref={headerRef}>
                             {/* rows */}
                             {cryptoData.slice(0, 99).map((coin, index) => (
                                 <tr key={coin.id}>
@@ -410,11 +410,11 @@ const HomeComnponent = () => {
                                         </div>
                                         
                                     </th>
-                                    <td>
+                                    <td className='sticky2nd'>
                                     <div className='cursor-pointer'>
                                         <Link href={`/${coin.id}`}>
                                             <img src={coin.image} className="sm:w-12 sm:h-12 w-9 h-9 inline-block mr-2" alt="ada" />
-                                            <span className='font-semibold inline-block align-middle uppercase sm:text-base text-sm mr-4'>{coin.id.substring(0, 18)}</span>
+                                            <span className='font-semibold inline-block align-middle uppercase sm:text-base text-sm mr-4 namesize'>{coin.id.substring(0, 18)}</span>
                                         </Link>
                                     </div>
                                     </td>
